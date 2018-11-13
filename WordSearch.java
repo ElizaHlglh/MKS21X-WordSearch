b0354c636b272f8db5d6d679ba36b7f7642cede0
ElizaHlglh <ewu4@stuy.edu>
2018-11-12 23:16:36 -0500

    WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -23,40 +23,117 @@ public class WordSearch{
         while(in.hasNext()){
           wordsToAdd.add(in.next());
         }
-
-        addWord(String word, row, col, int rowIncrement, int colIncrement);
+        data = new char[rows][cols];
+        for (int i = 0; i < rows; i++){
+          for (int x = 0; x < cols; x++){
+            data[i][x] = '_';
+          }
+        }
+        randgen = new Random();
+        seed = randgen.nextInt();
       }
       catch(FileNotFoundException e){
         System.out.println("File not found: " + fileName);
         //e.printStackTrace();
         System.exit(1);
       }
-
     }
 
     public WordSearch( int rows, int cols, String fileName, int randSeed){
-
+      try{
+        File f = new File(fileName);
+        Scanner in = new Scanner(f);
+        while(in.hasNext()){
+          wordsToAdd.add(in.next());
+        }
+        data = new char[rows][cols];
+        for (int i = 0; i < rows; i++){
+          for (int x = 0; x < cols; x++){
+            data[i][x] = '_';
+          }
+        }
+        randgen = new Random(randSeed);
+        seed = randSeed;
+      }
+      catch(FileNotFoundException e){
+        System.out.println("File not found: " + fileName);
+        //e.printStackTrace();
+        System.exit(1);
+      }
     }
 
     public String toString(){
       String puzzle = "";
       for (int i = 0; i < data.length; i++){
-        puzzle += "| "
+        puzzle += "| ";
         for (int x = 0; x < data[i].length; x++){
           puzzle += data[i][x] + "  ";
         }
         puzzle += "| \n";
       }
       puzzle += "Words: ";
-      for (int i = 0; i < wordsAdded.length; i++){
-        if (i == wordsAdded.length-1){
-          puzzle += wordsAdded[i] + "(seed: " + seed + ")";
+      for (int i = 0; i < wordsAdded.size(); i++){
+        if (i == wordsAdded.size()-1){
+          puzzle += wordsAdded.get(i) + "(seed: " + seed + ")";
         }
         else{
-          puzzle += wordsAdded[i] + ", ";
+          puzzle += wordsAdded.get(i) + ", ";
         }
       }
       return puzzle;
     }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+    *The word is added in the direction rowIncrement,colIncrement
+    *Words must have a corresponding letter to match any letters that it overlaps.
+    *
+    *@param word is any text to be added to the word grid.
+    *@param row is the vertical locaiton of where you want the word to start.
+    *@param col is the horizontal location of where you want the word to start.
+    *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+    *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+    *@return true when: the word is added successfully.
+    *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+    *        OR there are overlapping letters that do not match
+    */
+   private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+     try{
+       if (rowIncrement == 0 && colIncrement == 0){
+         return false;
+       }
+       int y = row;
+       int x = col;
+       for (int i = 0; i < word.length(); i++){ //check if addable
+         if (data[y][x] == word.charAt(i) || data[y][x] == '_'){
+           y+= rowIncrement;
+           x+= colIncrement;
+         }
+         else{
+           return false;
+         }
+       }
+
+       y = row;
+       x = col;
+       for (int i = 0; i < word.length(); i++){ //each character of word
+         data[y][x] = word.charAt(i);
+         y+= rowIncrement;
+         x+= colIncrement;
+       }
+       wordsAdded.add(word);
+       wordsToAdd.remove(word);
+       return true;
+     }
+     catch(IndexOutOfBoundsException e){
+        return false;
+      }
+   }
+
+   /*[rowIncrement,colIncrement] examples:
+    *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+    *[ 1,0] would add downwards because (row+1), with no col change
+    *[ 0,-1] would add towards the left because (col - 1), with no row change
+    */
+
 
 }
